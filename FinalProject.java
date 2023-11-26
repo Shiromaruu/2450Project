0ed02e9277da332086c9a47167140827749e7571
henraye <1henrytran@gmail.com>
2023-11-25 21:36:21 -0800

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -23,6 +23,41 @@ public class FinalProject {
         tabbedPane.addTab("Drink",drinkPanel);
         tabbedPane.addTab("Payment",paymentPanel);
 
+        //drink Panel
+        ImageIcon colaImg=new ImageIcon(getClass().getResource("cola.png"));
+        ImageIcon sprImg=new ImageIcon(getClass().getResource("sprite.png"));
+        JButton cola=new JButton(colaImg);
+        JButton sprite=new JButton(sprImg);
+
+        cola.setPreferredSize(new Dimension(100,100));
+        sprite.setPreferredSize(new Dimension(100,100));
+
+        drinkPanel.add(cola);
+        drinkPanel.add(sprite);
+
+        JScrollBar jVert=new JScrollBar(Adjustable.VERTICAL,0,50,0,300);
+        drinkPanel.add(jVert);
+
+
+        //payment panel
+        JRadioButton cc=new JRadioButton("Credit Card");
+        JRadioButton ven=new JRadioButton("Venmo");
+        JRadioButton cash=new JRadioButton("Cash");
+
+        cc.setFont(new Font("Molto",Font.ITALIC,25));
+        ven.setFont(new Font("Molto",Font.ITALIC,25));
+        cash.setFont(new Font("Molto", Font.ITALIC,25));
+
+        //add to button group for mutual exclusive function(one only)
+        ButtonGroup bg=new ButtonGroup();
+        bg.add(cc);
+        bg.add(ven);
+        bg.add(cash);
+
+        paymentPanel.add(cc);
+        paymentPanel.add(ven);
+        paymentPanel.add(cash);
+
         //creating labels
         JLabel shopping_cart = new JLabel("Shopping Cart");
         shopping_cart.setFont(new Font("Molto", Font.BOLD, 25));
